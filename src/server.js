fed9883c2440acb61cc5380b38d164aefa287d17
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-10-17 18:06:25 -0700

    working app edits to .env and server

--- src/server.js
+++ src/server.js
@@ -2,10 +2,19 @@ const knex = require('knex')
 const app = require('./app')
 
 const { PORT, DATABASE_URL } = require('./config')
+console.log(DATABASE_URL);
 
 const db = knex({
   client: 'pg',
-  connection: DATABASE_URL,
+  // connection: DATABASE_URL,
+  connection: {
+    port: '5432',
+    host: 'ec2-18-211-86-133.compute-1.amazonaws.com',
+    user: 'nspursnbrhcgvw',
+    password: "8eaa1311b06579f17f84a7028c1eb7bf7b9c09d0a47dcc3d9146156e1080ebe4",
+    database: 'dbvs1hk5r6jp43',
+    ssl: { rejectUnauthorized: false }
+  }
 })
 
 app.set('db', db)
